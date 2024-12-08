07001d94d95f51cf3175708827c016a0ed06e5a5
joshborut <jcborut@gmail.com>
2024-12-08 15:27:52 -0500

    Added minibatch functionality & backprop helper functions

--- train_nn.py
+++ train_nn.py
@@ -2,10 +2,23 @@
 import numpy as np
 import jax
 import jax.numpy as jnp
-from jax import random
+from jax import random as jrand
+import random as rand
+import random
+from pdb import set_trace
 from pdb import set_trace
 
 from dataset_generator import get_target, get_dataset, scatter_plot_xy, scatter_plot_two_datasets
+
+
+def mean_squared_error(labels, predictions):
+    print(type(0.5 * jnp.mean((labels - predictions) ** 2)))
+    return 0.5 * jnp.mean((labels - predictions) ** 2)
+
+
+def shuffle_dataset(examples, labels):
+    indices = np.random.permutation(len(examples))
+    return examples[indices], labels[indices]
 
 
 def get_average_dataset_deviation(predictions, labels):
@@ -18,17 +31,17 @@ def initialize_params(layer_sizes, key):
 
     Parameters:
     layer_sizes (list): List of integers specifying the size of each layer.
-    key (jax.random.PRNGKey): Random key for parameter initialization.
+    key (jax.rand.PRNGKey): Random key for parameter initialization.
 
     Returns:
     params (list): List of tuples containing (weights, biases) for each layer.
     """
     params = []
-    keys = random.split(key, len(layer_sizes) - 1)
+    keys = jrand.split(key, len(layer_sizes) - 1)
 
     for in_size, out_size, k in zip(layer_sizes[:-1], layer_sizes[1:], keys):
-        weight_key, bias_key = random.split(k)
-        weights = random.normal(weight_key, (in_size, out_size)) * jnp.sqrt(2.0 / in_size)
+        weight_key, bias_key = jrand.split(k)
+        weights = jrand.normal(weight_key, (in_size, out_size)) * jnp.sqrt(2.0 / in_size)
         biases = jnp.zeros(out_size)
         params.append((weights, biases))
 
@@ -56,17 +69,28 @@ def forward_pass(params, x):
     return output
 
 
-def training_loop(params, train_examples, train_labels, test_examples, test_labels, epochs):
+def training_loop(params, train_examples, train_labels, test_examples, test_labels, epochs, minibatch_size=20):
     for epoch in range(epochs):
-        print(f"Epoch #{epoch+1}")
-        # TODO: Pass through training set
+        train_examples, train_labels = shuffle_dataset(train_examples, train_labels)
+        starting_indices = range(0, len(train_examples), minibatch_size)
+        for starting_index in starting_indices:
+            minibatch_examples = train_examples[
+                                 starting_index: min(starting_index + minibatch_size, len(train_examples))]
+            minibatch_labels = train_labels[starting_index: min(starting_index + minibatch_size, len(train_labels))]
+
+            minibatch_predictions = forward_pass(params, jnp.reshape(jnp.array([minibatch_examples]),
+                                                                     (len(minibatch_labels), 1)))
+
+        print(f"Epoch #{epoch + 1}")
 
         # Assess on test set
-        predicted_test_labels = forward_pass(params, jnp.reshape(jnp.array([test_examples]), (len(test_labels),1)))
+        predicted_test_labels = forward_pass(params, jnp.reshape(jnp.array([test_examples]), (len(test_labels), 1)))
         print(get_average_dataset_deviation(jnp.squeeze(predicted_test_labels), test_labels))
 
     # Graph test results
-    scatter_plot_two_datasets(test_examples, test_labels, test_examples, predicted_test_labels, "True", "Predicted", "NN Performance")
+    scatter_plot_two_datasets(test_examples, test_labels, test_examples, predicted_test_labels, "True", "Predicted",
+                              "NN Performance")
+
 
 if __name__ == '__main__':
     dataset_size = 1000
@@ -81,10 +105,9 @@ if __name__ == '__main__':
 
     # Network setup
     layer_sizes = [1, 40, 40, 1]  # Input layer (1), two hidden layers (40 each), output layer (1)
-    key = random.PRNGKey(0)
+    key = jrand.PRNGKey(0)
 
     # Initialize parameters
     params = initialize_params(layer_sizes, key)
 
     training_loop(params, train_examples, train_labels, test_examples, test_labels, 1)
-
